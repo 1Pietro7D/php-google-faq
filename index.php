fd9272f2565040274c74db0ca41723bdeb7dad84
PietroDanieli <1pie7d@gmail.com>
2022-10-17 01:55:14 +0200

    da fare una funzione ricorsiva da riga 33 a 46, da stilizzare e fare un pò di refactoring, oltre che aggiungere i commenti

--- index.php
+++ index.php
@@ -12,7 +12,6 @@ $jsonStringLinks = file_get_contents("data/links.json");
 
   
 function getLinks($response, $linksData){
-   
   $risp= explode("§", $response);
   if (count($risp) == 1){echo $response;}
   else {
@@ -21,26 +20,37 @@ function getLinks($response, $linksData){
       if($i % 2  == 1){
           foreach ($linksData as $link) {
             if($risp[$i] == $link["id"])
-             {$risp[$i] = '<a href="' . $link["href"] . '">' . $link["text"] . '</a>';
-               
-              $risp = implode( "", $risp);
-             echo $risp;
-             
-          } 
-          }
-      }}
-      unset($i);
-      unset($risp);
-    } 
+            {$risp[$i] = '<a href="' . $link["href"] . '">' . $link["text"] . '</a>';
+           
+          }}}}
+          $risp = implode( "", $risp);
+    echo $risp; }
+
 
 };
 
 
   foreach ($faqData as $question) { ?>
     <h2><?= $question["question"] ?></h2>
-    <?php foreach ($question["response"] as $response) { ?>
-       <p style="margin-bottom: 1rem;"><?php getLinks($response, $linksData) ?></p>
-<?php  } }
+    <?php foreach ($question["response"] as $response) { 
+   if (!is_array($response)) {  ?>
+    <p style="margin-bottom: 1rem;"><?php getLinks($response, $linksData) ?></p>
+<?php } else{ ?> <ol> <?php foreach ($response as $point) { 
+    if (!is_array( $point )) { ?>
+       <li><?= $point ?></li>
+       <?php   } else { ?> <ol> <?php foreach ($point as $pit){ ?>
+        <li><?= $pit ?></li>
+         <?php  } ?>
+         </ol>
+         <?php }
+    ?>
+
+
+
+
+<?php } ?> </ol>
+<?php
+}}}
 ?>
 
 
