6ec7de242c0b4d2ebaf8a2e5ed834da9ff606011
PietroDanieli <1pie7d@gmail.com>
2022-10-17 01:05:11 +0200

    create structure and logic for first two question

---
+++ index.php
@@ -0,0 +1,46 @@
+<style>
+    a{
+        text-decoration: none;
+        color: #3367d6;
+    }
+</style>
+<?php
+$jsonStringData = file_get_contents("data/question.json");
+  $faqData = json_decode($jsonStringData, true); // True to indicate associative arrays
+$jsonStringLinks = file_get_contents("data/links.json");
+  $linksData= json_decode($jsonStringLinks, true);
+
+  
+function getLinks($response, $linksData){
+   
+  $risp= explode("§", $response);
+  if (count($risp) == 1){echo $response;}
+  else {
+    for ($i=0; $i < count($risp); $i++) { 
+    // un link per forza di cose sarà sempre in una posizione dispari nel array esploso
+      if($i % 2  == 1){
+          foreach ($linksData as $link) {
+            if($risp[$i] == $link["id"])
+             {$risp[$i] = '<a href="' . $link["href"] . '">' . $link["text"] . '</a>';
+               
+              $risp = implode( "", $risp);
+             echo $risp;
+             
+          } 
+          }
+      }}
+      unset($i);
+      unset($risp);
+    } 
+
+};
+
+
+  foreach ($faqData as $question) { ?>
+    <h2><?= $question["question"] ?></h2>
+    <?php foreach ($question["response"] as $response) { ?>
+       <p style="margin-bottom: 1rem;"><?php getLinks($response, $linksData) ?></p>
+<?php  } }
+?>
+
+
